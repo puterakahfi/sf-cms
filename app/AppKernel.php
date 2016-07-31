027c7a2cd5e561a91a2a1df983aedbecdea5ad2f
puterakahfi <puterakahfi@gmail.com>
2016-07-31 17:01:53 +0700

    integrated easyAdminBundle and FOSUserBundle for rapid admin development

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+			new FOS\UserBundle\FOSUserBundle(),
+			new AppBundle\AppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
